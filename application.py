b2a3d5883641054105684f4ccad9a7514189a84b
rlondt <rlondt@minjak.nl>
2017-01-25 02:13:55 +0100

    my first model

--- application.py
+++ application.py
@@ -1,8 +1,7 @@
-import json
 from sklearn.externals import joblib
 import pandas as pd
 
-from flask import Flask, url_for, send_from_directory, request
+from flask import Flask, url_for, send_from_directory, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
@@ -20,12 +19,30 @@ def api_article(articleid):
 
 @app.route('/api/v1/predict')
 def api_v1_predict():
-    json_ = request.json
-    query_df = pd.DataFrame(json_)
-    query = pd.get_dummies(query_df)
-    prediction = clf.predict(query)
-    return jsonify({'prediction': list(prediction)})
+    query_df = pd.DataFrame()
+    multi_dict = request.args
+    for key in multi_dict:
+        # multi_dict.get(key)
+        # multi_dict.getlist(key)
+        # query_df[key] = query_df.index
+        if key in ('delivery_method','body_length','sale_duration'):
+            query_df.loc[0, key] = float(multi_dict.get(key))
+
+    # print(query_df.dtypes)
+    # prediction = clf.predict(query_df)
+    # query_df = pd.DataFrame()
+    # query_df.loc[0, 'delivery_method'] = 1.0
+    # query_df.loc[0, 'body_length'] = 2113
+    # query_df.loc[0, 'sale_duration'] = 16.0
+    # print(query_df.dtypes)
+    # print(prediction.item(0))
+    p = clf.predict_proba(query_df)[0, 1]
+    l = float(p > 0.5)
+    print(p)
+    return jsonify({"sample_uuid":  multi_dict.get("sample_uuid"), "probability": p, "label": l})
+
+    #return jsonify({'prediction': list(prediction)})
 
 if __name__ == '__main__':
-    clf = joblib.load('filename.pkl')
+    clf = joblib.load('filename2.pkl')
     app.run(host="0.0.0.0")
